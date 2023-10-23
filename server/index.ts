ce0715b01e663542dfa4ff9d0a62195e0fd9cb85
Mahmoud Abdulshakour <mahmoud.ali1842001@gmail.com>
2023-10-23 09:59:31 +0300

    Add typescript-prisma dependencies

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,16 @@
 import express from "express";
-
+import { PrismaClient } from '@prisma/client'
 const app = express();
+const prisma = new PrismaClient()
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(8000, async () => {
+  console.log("Server running on port 8000\n")
+  console.log(process.env.DATABASE_URL);
+
+  console.log(await prisma.user.findMany());
+});
