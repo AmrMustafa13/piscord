96c2aa7ffe8888c3e3bdb009062a9ba099c3a2eb
baarayy <albaraa.ah26@icloud.com>
2023-10-23 11:08:52 +0300

    Create user table

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,13 @@
 import express from "express";
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 const app = express();
-const prisma = new PrismaClient()
-
+const prisma = new PrismaClient();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
 app.listen(8000, async () => {
-  console.log("Server running on port 8000\n")
+  console.log("Server running on port 8000\n");
   console.log(process.env.DATABASE_URL);
 
   console.log(await prisma.user.findMany());
