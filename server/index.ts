b2a35d7c51ce49db01bc6237daed6f8038d10637
Mahmoud Abdulshakour <mahmoud.ali1842001@gmail.com>
2023-10-23 18:46:15 +0300

    Add signup endpoint

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,19 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client";
-const app = express();
-const prisma = new PrismaClient();
+import { signupController } from "./controllers/userController";
+import { prisma } from "./db";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const app = express();
+app.use(express.json());
+
+
+
+
+
+app.post("/signup",signupController);
+app.get("/",async (req,res)=>{
+  console.log(await prisma.user.findMany());
 });
+
 app.listen(8000, async () => {
   console.log("Server running on port 8000\n");
-  console.log(process.env.DATABASE_URL);
-
-  console.log(await prisma.user.findMany());
 });
