31ff3bd03f8905a576a21b4987d8725d6f09807f
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-10-25 04:53:13 +0300

    Add Icons Styling in Typing Area

--- client/src/components/Chat/TypingField.tsx
+++ client/src/components/Chat/TypingField.tsx
@@ -1,4 +1,8 @@
-import React, { FC } from "react";
+import { FC } from "react";
+import { AiFillGift, AiFillPlusCircle } from "react-icons/ai";
+import { HiMiniGif } from "react-icons/hi2";
+import { LuSticker } from "react-icons/lu";
+import { BsFillEmojiSmileFill } from "react-icons/bs";
 
 interface Props {
   channelTitle: string;
@@ -6,11 +10,16 @@ interface Props {
 const TypingField: FC<Props> = ({ channelTitle }) => {
   return (
     <div className="h-[68px] flex-shrink-0 px-4">
-      <div className="bg-[#383a40] p-[11px] flex rounded-lg">
+      <div className="bg-[#383a40] p-[11px] flex items-center gap-3 rounded-lg">
+        <AiFillPlusCircle className="typing-icon" />
         <input
           className="border-none outline-none flex-1 bg-transparent"
           placeholder={`Message #${channelTitle}`}
         />
+        <AiFillGift className="typing-icon" />
+        <HiMiniGif className="typing-icon" />
+        <LuSticker className="typing-icon" />
+        <BsFillEmojiSmileFill className="typing-icon" />
       </div>
     </div>
   );
