e1dc477525b2776509c70cc4e03a6a9e4f82d356
Mahmoud Abdulshakour <mahmoud.ali1842001@gmail.com>
2023-10-28 12:55:43 +0200

    Add server routes

---
+++ server/src/routes/serverRoute.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { addUserToServerController, createServerController, deleteServerController, editServerController, getServerController } from "../controllers/serverController";
+import { authToken } from "../middlewares/authToken";
+const serverRouter = Router();
+serverRouter.post("/server",authToken, createServerController);
+serverRouter.delete("/server/:serverId",authToken,deleteServerController );
+serverRouter.put("/server/:serverId",authToken,editServerController );
+serverRouter.get("/server/:serverId",authToken,getServerController);
+serverRouter.post("/server/:serverId/:userId",authToken,addUserToServerController );
+export default serverRouter;
