c0f6d00dce6cad77bb2349e0ab56e823de1e875a
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-10-28 15:58:30 +0200

    Add Responsive Design for Services section

--- client/src/components/Home/Services.tsx
+++ client/src/components/Home/Services.tsx
@@ -7,13 +7,13 @@ const Services = () => {
   return (
     <>
       <section>
-        <div className="max-w-[1260px] m-auto flex justify-between items-center px-[40px] py-[120px] gap-[10%]">
-          <img src={service1} className="min-w-0" />
-          <div className="w-[394px] flex-shrink-0">
-            <h3 className="font-extrabold text-5xl leading-[57.6px]">
+        <div className="max-w-[1260px] m-auto flex flex-wrap md:flex-nowrap justify-between items-center px-[40px] py-[120px]">
+          <img src={service1} className="flex-0 md:flex-1 lg:flex-[7] min-w-0" />
+          <div className="flex-0 md:flex-1 lg:flex-[4]">
+            <h3 className="font-extrabold text-[clamp(20px,4vw,40px)] leading-[57.6px]">
               Create an invite-only place where you belong
             </h3>
-            <p className="text-[20px] font-normal mt-6 font-gg-sans text-[#272b2e]">
+            <p className="text-[clamp(16px,2vw,20px)] font-normal mt-6 font-gg-sans text-[#272b2e]">
               Discord servers are organized into topic-based channels where you can collaborate,
               share, and just talk about your day without clogging up a group chat.
             </p>
@@ -21,23 +21,27 @@ const Services = () => {
         </div>
       </section>
       <section className="bg-[#f6f6f6]">
-        <div className="max-w-[1260px] m-auto flex justify-between items-center px-[40px] py-[120px] gap-[10%]">
-          <div className="w-[394px] flex-shrink-0">
-            <h3 className="font-extrabold text-5xl leading-[57.6px]">Where hanging out is easy</h3>
-            <p className="text-[20px] font-normal mt-6 font-gg-sans text-[#272b2e]">
+        <div className="max-w-[1260px] m-auto flex flex-wrap-reverse md:flex-nowrap justify-between items-center px-[40px] py-[120px]">
+          <div className="flex-0 md:flex-1 lg:flex-[4]">
+            <h3 className="font-extrabold text-[clamp(20px,4vw,40px)] leading-[57.6px]">
+              Where hanging out is easy
+            </h3>
+            <p className="text-[clamp(16px,2vw,20px)] font-normal mt-6 font-gg-sans text-[#272b2e]">
               Grab a seat in a voice channel when you’re free. Friends in your server can see you’re
               around and instantly pop in to talk without having to call.
             </p>
           </div>
-          <img src={service2} className="min-w-0" />
+          <img src={service2} className="flex-0 md:flex-1 lg:flex-[7] min-w-0" />
         </div>
       </section>
       <section>
-        <div className="max-w-[1260px] m-auto flex justify-between items-center px-[40px] py-[120px] gap-[10%]">
-          <img src={service3} className="min-w-0" />
-          <div className="w-[394px] flex-shrink-0">
-            <h3 className="font-extrabold text-5xl leading-[57.6px]">From few to a fandom</h3>
-            <p className="text-[20px] font-normal mt-6 font-gg-sans text-[#272b2e]">
+        <div className="max-w-[1260px] m-auto flex flex-wrap md:flex-nowrap justify-between items-center px-[40px] py-[120px]">
+          <img src={service3} className="flex-0 md:flex-1 lg:flex-[7] min-w-0" />
+          <div className="flex-0 md:flex-1 lg:flex-[4]">
+            <h3 className="font-extrabold text-[clamp(20px,4vw,40px)] leading-[57.6px]">
+              From few to a fandom
+            </h3>
+            <p className="text-[clamp(16px,2vw,20px)] font-normal mt-6 font-gg-sans text-[#272b2e]">
               Get any community running with moderation tools and custom member access. Give members
               special powers, set up private channels, and more.
             </p>
@@ -45,11 +49,11 @@ const Services = () => {
         </div>
       </section>
       <section>
-        <div className="max-w-[1260px] m-auto px-[40px] py-[120px] text-center">
-          <h3 className="font-abc-ginto-nord font-extrabold text-[40px] ">
+        <div className="max-w-[1260px] m-auto px-[40px] py-[120px] md:text-center">
+          <h3 className="font-abc-ginto-nord font-extrabold text-[clamp(20px,4vw,40px)] ">
             Reliable tech for staying close
           </h3>
-          <p className="text-[20px] font-normal mt-6 font-gg-sans text-[#272b2e]">
+          <p className="text-[clamp(16px,2vw,20px)] font-normal mt-6 font-gg-sans text-[#272b2e]">
             Low-latency voice and video feels like you’re in the same room. Wave hello over video,
             watch friends stream their games, or gather up and have a drawing session with screen
             share.
