7d45a28794aa7867641d57c243e35e8630b50eb3
AmrMustafa13 <amrmostafa1311@gmail.com>
2023-11-05 22:42:56 +0200

    fix protected route

--- client/src/components/Protected.tsx
+++ client/src/components/Protected.tsx
@@ -1,12 +1,15 @@
 import { Navigate } from "react-router-dom";
+import { AuthContext } from "@/contexts/auth";
+import { useContext } from "react";
 
 interface ProtectedProps {
-  isSignedIn: string | null;
   children: JSX.Element;
 }
 
-const Protected = ({ isSignedIn, children }: ProtectedProps) => {
-  if (!isSignedIn) {
+const Protected = ({ children }: ProtectedProps) => {
+  const { user } = useContext(AuthContext);
+
+  if (!user) {
     return <Navigate to="/" replace />;
   }
   return children;
